623f1794efaed8a4af3b065f7c0673eeac45d864
Bifi92 <bifi@pop-os.localdomain>
2022-07-24 08:50:44 -0300

    inclusao da listagem e exclusao de repos

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,6 +1,6 @@
 import React, { useCallback, useState } from 'react';
-import {Container, Form, SubmitButton} from './styles';
-import {FaGithub, FaPlus, FaSpinner} from 'react-icons/fa';
+import {Container, Form, SubmitButton, List, DeleteButton} from './styles';
+import {FaBars, FaGithub, FaPlus, FaSpinner, FaTrash} from 'react-icons/fa';
 import api from '../../services/api';
 
 export default function Main(){
@@ -44,6 +44,13 @@ export default function Main(){
         
     }, [newRepo, repositorios]);
 
+    const handleDelete = useCallback((repo)=>{
+
+        const find = repositorios.filter(r => r.name !== repo);
+        setRepositorios(find);
+
+    }, [repositorios]);
+
     return (
         <Container>
             <h1>
@@ -69,6 +76,22 @@ export default function Main(){
                     
                 </SubmitButton>
             </Form>
+
+            <List>
+                {repositorios.map(repo => (
+                    <li key={repo.name}>
+                        <span>
+                            <DeleteButton onClick={()=> handleDelete(repo.name)}>
+                                <FaTrash size={14}/>
+                            </DeleteButton>
+                            {repo.name}
+                        </span>
+                        <a href="">
+                            <FaBars size={20}/>
+                        </a>
+                    </li>
+                ))}
+            </List>
 
         </Container>
     )
